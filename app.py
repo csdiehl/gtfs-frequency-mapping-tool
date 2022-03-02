203ce6633d3fd908fef57d51542f876ce5ff6c09
csdiehl <59377951+csdiehl@users.noreply.github.com>
2022-03-02 13:11:38 -0800

    added attributes for shapefile

--- app.py
+++ app.py
@@ -40,10 +40,12 @@ questions = [{
   'choices': net_choices
 }]
 
+#Save user preferences
 answers = prompt(questions)
 network = answers['network']
 GTFS = answers['GTFS']
 selected_day = answers['day_type']
+chosen_direction = answers['direction']
 
 if network == 'Enter Route List': 
   route_choices = input('Route List: ')
@@ -60,6 +62,7 @@ while True:
 start = time.time()
 
 #Importing the GTFS
+print('')
 print("Importing GTFS...")
 routes, stops, stop_times, trips, shapes = gtfs.import_gtfs('./feeds/' + GTFS, busiest_date = True if selected_day == 'Busiest Day' else False)
 print('GTFS imported in ' + str(round((time.time() - start) / 60, 3)) + ' minutes')
@@ -93,7 +96,6 @@ if len(shapes) == 0:
 
 #might need to find more flexible way to filter just bus routes that works for multiple agencies
 
-
 #Creating subsegments for each route
 print('Splitting routes into segments...')
 freq_start = time.time()
@@ -110,17 +112,23 @@ seg_freq = gtfs.segments_freq(segments_gdf, stop_times, routes, cutoffs = cutoff
 
 #Filter for time window, combine frequency along shared segments
 if answers['direction'] != 'Both Ways': 
-  combined = seg_freq[seg_freq.dir_id == answers['direction']].groupby(['segment_id', 'window', 's_st_id', 's_st_name', 'e_st_name']).agg({'frequency': 'sum', 'ntrips': 'sum', 'geometry': 'first'}).reset_index()
-  combined = gpd.GeoDataFrame(combined, crs="EPSG:3857")
+  combined = seg_freq[seg_freq.dir_id == chosen_direction]
 
 else: 
-  combined = seg_freq[(seg_freq.route_name == 'All lines')]
-  combined.drop(columns = ['route_id', 'route_name', 'dir_id'], inplace = True)
+  combined = seg_freq[(seg_freq.route_name != 'All lines')]
+
+
+combined.groupby(['segment_id', 'window', 's_st_id', 's_st_name', 'e_st_name']).agg({'route_name': list, 'frequency': 'sum', 'ntrips': 'sum', 'geometry': 'first'}).reset_index()
+combined['route_name'] = combined.route_name.apply(lambda x: ', '.join(map(str, x))) #turn list into string so can be saved in shapefile
+combined['day_type'] = selected_day
+combined['direction'] = chosen_direction
+combined = gpd.GeoDataFrame(combined, crs="EPSG:3857")
+#combined.drop(columns = ['route_id', 'route_name', 'dir_id'], inplace = True)
 
 #calculate number of hours in each time window and headways (minute)
 diff = pd.to_datetime(combined.window.str.split("-", expand = True)[1], format = '%H:%M') - pd.to_datetime(combined.window.str.split("-", expand = True)[0], format = '%H:%M')
 hours = combined['hours'] = diff.dt.total_seconds() / 3600
-combined['headway'] = round((1 / (combined.frequency / hours)) * 60, 1)
+combined['headway_mins'] = round((1 / (combined.frequency / hours)) * 60, 1)
 
 #Ask for file name
 while True: 
@@ -134,7 +142,7 @@ print('Saving results...')
 
 gtfs.save_gdf(combined, file_name, shapefile = True, geojson = False)
 #Save a basic map for reference / error checking
-ax = combined.plot(figsize=(20, 20), column = 'frequency', cmap = 'inferno', 
+ax = combined.plot(figsize=(20, 20), column = 'headway_mins', cmap = 'inferno', 
             scheme = 'NaturalBreaks', k = 6, legend = True, alpha = .7, markersize = 2)
 ax.set_axis_off()
 plt.savefig('map.png')
